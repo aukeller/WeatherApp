283378ff4258dbbe9c6b5524483991612c8e0ef8
Austin Keller <austinkllr@gmail.com>
2022-04-03 21:20:58 -0700

    fixed module import error, have core functionality complete

---
+++ src/WeatherModule.js
@@ -0,0 +1,41 @@
+export default function WeatherModule() {
+    async function getWeatherData(location) {
+        const APIKey = 'e5f7be41d958d90ca7cb6387141e813e';
+        
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${APIKey}`, {mode: 'cors'});
+        const responseData = await response.json();
+    
+        return responseData;
+    }
+
+    async function getTemperature(location, unit) {
+        const responseData = await getWeatherData(location);
+        const temp = responseData.main.temp;
+    
+        if (unit == "F") {
+            console.log(convertToF(temp));
+        } else {
+            console.log(convertToC(temp));
+        }
+        
+    }
+
+    async function getDesc(location) {
+        const responseData = await getWeatherData(location);
+        const desc = responseData.weather[0].main;
+    
+        console.log(desc);
+    }
+
+    const convertToF = (temp) => {
+        return Math.round(((temp - 273.15) * (9 / 5) + 32));
+    }
+    
+    const convertToC = (temp) => {
+        return Math.round(temp - 273.15);
+    }
+
+    return { getTemperature, getDesc};
+    
+}
+
--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-async function getWeatherData(location) {
-    const APIKey = 'e5f7be41d958d90ca7cb6387141e813e';
-    
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${APIKey}`);
-    const responseData = await response.json();
+import  WeatherModule from "./WeatherModule";
 
-    return responseData;
-}
-
-async function getTemperature(location, unit) {
-    const responseData = await getWeatherData(location);
-    const temp = responseData.main.temp;
-
-    if (unit == "F") {
-        console.log(convertToF(temp));
-    } else {
-        console.log(convertToC(temp));
-    }
-    
-}
-
-async function getDesc(location)
-
-const convertToF = (temp) => {
-    return Math.round(((temp - 273.15) * (9 / 5) + 32));
-}
-
-const convertToC = (temp) => {
-    return Math.round(temp - 273.15);
-}
-
+const weather = WeatherModule();
+weather.getDesc('San Ramon', 'F');
