b092e107304983e916765ecd7c5f6a366dee6981
Austin Keller <austinkllr@gmail.com>
2022-04-06 22:09:36 -0700

    core functionality complete. Able to convert temp unit

--- src/index.js
+++ src/index.js
@@ -9,12 +9,23 @@ const getWeatherBtn = document.querySelector('button');
 const tempDisplay = document.querySelector('.temperature');
 const tempDesc = document.querySelector('.description');
 
+const radioBtns = document.querySelectorAll('input[type="radio"');
+
 getWeatherBtn.addEventListener('click', function() {
+    const unit = setUnit();
     weather.getAPIData(locationInput.value)
     .then((response) => {
-        const weatherData = weather.getWeatherData(response, 'F');
+        const weatherData = weather.getWeatherData(response, unit);
 
-        tempDisplay.textContent = weatherData.temperatue;
+        tempDisplay.textContent = `${weatherData.temperatue}\u00B0`;
         tempDesc.textContent = weatherData.description;
     });
 });
+
+function setUnit() {
+    for (const radio of radioBtns) {
+        if (radio.checked) {
+            return radio.value;
+        }
+    }
+}
